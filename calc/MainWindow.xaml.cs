ecd2e56d0c75f41c33c9f7227e08261d1beb1ad7
saschek <saschek2000@yandex.ru>
2019-10-09 18:34:43 +0300

    calc

--- calc/MainWindow.xaml.cs
+++ calc/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace calc
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        ArrayList ResultList = new ArrayList();
         public MainWindow()
         {
             InitializeComponent();
@@ -29,81 +30,130 @@ namespace calc
         private void One_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 1;
+            ResultList.Add(1);
         }
 
         private void Two_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 2;
+            ResultList.Add(2);
         }
 
         private void Three_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 3;
+            ResultList.Add(3);
         }
 
         private void Four_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 4;
+            ResultList.Add(4);
         }
 
         private void Five_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 5;
+            ResultList.Add(5);
         }
 
         private void Six_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 6;
+            ResultList.Add(6);
         }
 
         private void Seven_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 7;
+            ResultList.Add(7);
         }
 
         private void Eight_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 8;
+            ResultList.Add(8);
         }
 
         private void Nine_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 9;
+            ResultList.Add(9);
         }
 
         private void Zero_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 0;
+            ResultList.Add(0);
         }
 
         private void Equally_Click(object sender, RoutedEventArgs e)
         {
-            
+            string Number ="";
+            double Result = 0;
+            int NumInt;
+            double NumDouble;
+            foreach(var Item in ResultList)
+            {
+                if (Item.GetType().Name.ToString() == "String")
+                {
+                    try
+                    {
+                        NumDouble = double.Parse(Number);
+                        Result += NumDouble;
+                    }
+                    catch
+                    {
+                        NumInt= int.Parse(Number);
+                        Result += NumInt ;
+                    }
+     
+                    
+
+                    Number = "";
+                }
+                else
+                {
+                    Number += Item;
+                }
+            }
+            MessageBox.Show(Result.ToString());
         }
 
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "+";
+            ResultList.Add("+");
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "-";
+            ResultList.Add("-");
         }
 
         private void Multiply_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "*";
+            ResultList.Add("*");
         }
 
         private void Divide_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "/";
+            ResultList.Add("/");
         }
 
         private void AllClear_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text = "";
+            ResultList.Clear();
+        }
+
+        private void Point_Click(object sender, RoutedEventArgs e)
+        {
+            CalcBox.Text += ".";
+            ResultList.Add(".");
         }
     }
 }
