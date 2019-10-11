212fbf2772cccc08518463cdd3e4b578ed77c8e4
saschek <saschek2000@yandex.ru>
2019-10-11 13:31:25 +0300

    calc

---
+++ calc/Logic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace calc
+{
+    class Logic
+    {
+
+        public static double Counting(TextBox CalcBox)
+        {
+            ArrayList ResultList = new ArrayList();
+            double Result = 0;
+            string number;
+            ResultList.Clear();
+            foreach (var item in CalcBox.Text)
+            {
+                ResultList.Add(item);
+            }
+            ResultList.Add('=');
+            char FirstElement = '+';
+            char TwoElement = '+';
+            int i = 0;
+            for (int k = 0; k < ResultList.Count; k++)
+            {
+                char Item = char.Parse(ResultList[k].ToString());
+
+                if (Item == '+' || Item == '-' || Item == '*' || Item == '/' || Item == '=')
+                {
+
+                    TwoElement = FirstElement;
+                    FirstElement = Item;
+                    number = "";
+
+                    for (int l = i; l < k; l++)
+                    {
+                        number += ResultList[l].ToString();
+                    }
+                    switch (TwoElement)
+                    {
+                        case '+':
+                            Result += double.Parse(number);
+                            break;
+                        case '-':
+                            Result -= double.Parse(number);
+                            break;
+                        case '/':
+                            if (double.Parse(number) == 0)
+                            {
+                                MessageBox.Show("Деление на ноль невозможно!");
+                            }
+                            else
+                            {
+                                Result /= double.Parse(number);
+                            }
+                            break;
+                        case '*':
+                            Result *= double.Parse(number);
+                            break;
+                    }
+                    i = k + 1;
+                }
+            }
+            return Result;
+        }
+
+    }
+}
--- calc/MainWindow.xaml.cs
+++ calc/MainWindow.xaml.cs
@@ -21,49 +21,9 @@ namespace calc
     /// </summary>
     public partial class MainWindow : Window
     {
-        ArrayList ResultList = new ArrayList();
-        double Result = 0;
-        char sym = '+';
 
-        //public void counting()
-        //{
-        //    foreach(var Item in CalcBox.Text)
-        //    {
-        //        if(Item == '+' || Item == '-' || Item == '*' || Item == '/' || Item == '=')
-        //        {
 
-        //        }
-        //    }
-        //}
-
-        public void Val()
-        {
-            string res = "";
-            double rs = 0;
-            foreach (var itm in ResultList)
-            {
-                res += itm.ToString();
-            }
-            try { rs = double.Parse(res); } catch { rs = 0; }
-            switch (sym)
-            {
-                case '+':
-                    Result += rs;
-                    break;
-                case '-':
-                    Result -= rs;
-                    break;
-                case '*':
-                    Result *= rs;
-                    break;
-                case '/':
-                    Result /= rs;
-                    break;
-            }
-            sym = '+';
-            ResultList.Clear();
-        }
-
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -72,117 +32,98 @@ namespace calc
         private void One_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 1;
-            ResultList.Add(1);
         }
 
         private void Two_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 2;
-            ResultList.Add(2);
         }
 
         private void Three_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 3;
-            ResultList.Add(3);
         }
 
         private void Four_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 4;
-            ResultList.Add(4);
         }
 
         private void Five_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 5;
-            ResultList.Add(5);
         }
 
         private void Six_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 6;
-            ResultList.Add(6);
         }
 
         private void Seven_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 7;
-            ResultList.Add(7);
         }
 
         private void Eight_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 8;
-            ResultList.Add(8);
         }
 
         private void Nine_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 9;
-            ResultList.Add(9);
         }
 
         private void Zero_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += 0;
-            ResultList.Add(0);
         }
 
         private void Equally_Click(object sender, RoutedEventArgs e)
         {
-            Val();
-            MessageBox.Show(Result.ToString());
+            try
+            {
+                CalcBox.Text = Logic.Counting(CalcBox).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
-            CalcBox.Text += "+";
-          Val();
-            sym = '+';
-
+                CalcBox.Text += "+";
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "-";
-        Val();
-            sym = '-';
-
         }
 
         private void Multiply_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "*";
-         Val();
-            sym = '*';
-
         }
 
         private void Divide_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text += "/";
-           Val();
-            sym = '/';
-
         }
-
         private void AllClear_Click(object sender, RoutedEventArgs e)
         {
             CalcBox.Text = "";
-            ResultList.Clear();
-            Result = 0;
         }
-
         private void Point_Click(object sender, RoutedEventArgs e)
         {
-            CalcBox.Text += ".";
-            ResultList.Add(",");
+            CalcBox.Text += ",";
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Backspace_Click(object sender, RoutedEventArgs e)
         {
-   //         if(e.Key.ToString() == "0") { MessageBox.Show(e.Key.ToString()); }
+            if (CalcBox.Text.Length >0)
+            CalcBox.Text = CalcBox.Text.Substring(0, CalcBox.Text.Length - 1);
         }
     }
 }
